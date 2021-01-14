bb191dd1ed68fd154d9966b78ef61ee22e9eaf24
khl6235 <khl6235@naver.com>
2021-01-14 19:22:09 +0900

    user information restore in sessionStorage

--- front/form-app/src/component/user/LoginComponent.jsx
+++ front/form-app/src/component/user/LoginComponent.jsx
@@ -1,99 +1,111 @@
-import React, {Component} from 'react';
-import ApiService from '../../ApiService';
+import React, { Component } from "react";
+import ApiService from "../../ApiService";
 
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { TextField } from '@material-ui/core';
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { TextField } from "@material-ui/core";
 
-class LoginComponent extends Component{
-                                                                                                                                                                                                                                                                                                                              
-    constructor(props){
-        super(props);
+class LoginComponent extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            userIdx: '',
-            id: '',
-            password: ''
-        };
-    }
+    this.state = {
+      userIdx: "",
+      id: "",
+      password: "",
+    };
+  }
 
-    onChange = (e) =>{
-        this.setState({
-            [e.target.name]: e.target.value
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  logIn = () => {
+    if (this.state.id.length > 0 && this.state.password.length > 0) {
+      ApiService.login(this.state)
+        .then((res) => {
+          if (res && res.data) {
+            this.setState({
+              userIdx: res.data.userIdx,
+              id: res.data.id,
+              password: res.data.password,
+            });
+            this.storeUser();
+          } else {
+            alert("존재하지 않는 사용자입니다.");
+          }
+        })
+        .catch((err) => {
+          console.log("login error!", err.response);
         });
+    } else {
+      alert("정보를 입력해주세요.");
     }
+  };
 
-    logIn = (e) => {
-        e.preventDefault();
+  storeUser = () => {
+    const { userIdx, id, password } = this.state;
 
-        let user = {
-            userIdx: this.state.userIdx,
-            id: this.state.id,
-            password: this.state.password
-        }
+    window.sessionStorage.setItem("userIdx", userIdx);
+    window.sessionStorage.setItem("id", id);
+    this.props.history.push("/forms");
+  };
 
-        if(this.state.id.length > 0 && this.state.password.length > 0){
-            ApiService.login(user)
-            .then(res => {
-                console.log(res.data);
-                if(res && res.data){
-                    this.setState({
-                        userIdx: res.data.userIdx,
-                        id: res.data.id,
-                        password: res.data.password
-                    })
-                    this.props.history.push('/forms');
-                }
-                else{
-                    alert('존재하지 않는 사용자입니다.');
-                }
-                
-            })
-            .catch(err => {
-                console.log("login error!", err.response);
-            })            
-        }
-        else{
-            alert('정보를 입력해주세요.');
-        }
-    }
+  render() {
+    return (
+      <div style={style}>
+        <Typography variant="h4" style={style2}>
+          LOGIN
+        </Typography>
+        <form style={loginContainer}>
+          <TextField
+            type="text"
+            placeholder="ID"
+            fullWidth
+            margin="normal"
+            value={this.state.id}
+            name="id"
+            onChange={this.onChange}
+          />
 
-    render(){
-        return(
-            <div style={style}>
-                <Typography variant="h4"  style={style2}>LOGIN</Typography>
-                <form style={loginContainer}>
-                    <TextField type="text" placeholder="ID" fullWidth margin="normal" 
-                    value={this.state.id} name="id" onChange={this.onChange}/>
-                    
-                    <TextField type="password" placeholder="PASSWORD" fullWidth margin="normal" 
-                    value={this.state.password} name="password" onChange={this.onChange}/>
+          <TextField
+            type="password"
+            placeholder="PASSWORD"
+            fullWidth
+            margin="normal"
+            value={this.state.password}
+            name="password"
+            onChange={this.onChange}
+          />
 
-                    <Button variant="contained" color="primary" onClick={this.logIn}>login</Button>
-                </form>
-                {/* <FormListComponent userIdx={this.state.userIdx} id={this.state.id}/> */}
-            </div>
-        );
-    }
+          <Button variant="contained" color="primary" onClick={this.logIn}>
+            login
+          </Button>
+        </form>
+        {/* <FormListComponent userIdx={this.state.userIdx} id={this.state.id}/> */}
+      </div>
+    );
+  }
 }
 
 const style = {
-    width: '300px',
-    margin: '0 auto'
-}
+  width: "300px",
+  margin: "0 auto",
+};
 
-const style2={
-    marginTop: '60px',
-    display: 'flex',
-    justifyContent: 'center'
-}
+const style2 = {
+  marginTop: "60px",
+  display: "flex",
+  justifyContent: "center",
+};
 
 const loginContainer = {
-    marginTop: '50px',
-    display: 'flex',
-    flexFlow: 'row wrap',
-    justifyContent: 'center'
-    
-}
+  marginTop: "50px",
+  display: "flex",
+  flexFlow: "row wrap",
+  justifyContent: "center",
+};
 
 export default LoginComponent;
